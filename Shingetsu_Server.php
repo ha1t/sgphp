c3423dc54477ac68f175ca7fe1e63019468bcd86
ha1t <halt.feits@gmail.com>
2011-07-19 12:59:15 +0900

    join(),bye()の実装

--- Shingetsu_Server.php
+++ Shingetsu_Server.php
@@ -20,6 +20,38 @@ class Shingetsu_Server
 
     public function recent()
     {
+        foreach (glob('data/*') as $file) {
+        }
+    }
+
+    // @TODO 相手にpingが通るか確認する
+    public function join($node)
+    {
+        $node = str_replace('+', '/', $node);
+
+        $lines = file('nodelist.txt');
+        if (!in_array($node, $lines)) {
+            $lines[] = $node;
+            file_put_contents('nodelist.txt', implode('', $lines));
+        }
+
+        echo 'WELCOME';
+    }
+
+    // @TODO 相手にpingが通るか確認する
+    public function bye($node)
+    {
+        $node = str_replace('+', '/', $node);
+
+        $lines = file('nodelist.txt');
+        foreach ($lines as $key => $line) {
+            if ($line == $node) {
+                unset($lines[$key]);
+            }
+        }
+        file_put_contents('nodelist.txt', implode('', $lines));
+
+        echo 'BYEBYE';
     }
 
     public function have()
