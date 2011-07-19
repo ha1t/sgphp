bab28c0117cf4f56d89340a79275cbc382f16e83
ha1t <halt.feits@gmail.com>
2011-07-19 16:59:07 +0900

    get()でfetch()使ってなかったので使うようにした。

--- Shingetsu_Client.php
+++ Shingetsu_Client.php
@@ -2,10 +2,9 @@
 // ディスク消費が大きいので他人のURLに振り向けてでもいいかも(Coral CDN)
 // http://shingetsu.info/protocol/protocol-0.7.d1
 
-$server = 'bbs.shingetsu.info:80/server.cgi';
-$server = 'rep4649.ddo.jp:80/server.cgi';
-$server = 'sg.sabaitiba.com:49494/server.cgi';
 $server = '163.43.161.96:8000/server.cgi';
+$server = 'sg.sabaitiba.com:49494/server.cgi';
+$server = 'rep4649.ddo.jp:80/server.cgi';
 $server = 'sgphp.project-p.jp/server.php';
 
 class Shingetsu_Client
@@ -86,7 +85,7 @@ class Shingetsu_Client
         if ($timestamp !== false) {
             $url .= "/{$timestamp}";
         }
-        $response = trim(file_get_contents($url));
+        $response = $this->fetch($url);
 
         return $response;
     }
@@ -105,12 +104,31 @@ $s = new Shingetsu_Client($server);
 //$result = $s->node(); var_dump($result); exit;
 
 $my_node = str_replace('/', '+', ':80/server.php');
-$result = $s->join($my_node); var_dump($result); exit;
+//$result = $s->join($my_node); var_dump($result); exit;
 
 //$result = $s->have('thread_6F70657261'); var_dump($result); exit;
 //$result = $s->have('thread_E69CAC'); var_dump($result); exit;
 
 //$files = $s->recent(); var_dump($files);
+
+function get_thread($s, $filename)
+{
+    // var_dump($s->have($filename)); exit;
+
+    if (!$s->have($filename)) {
+        var_dump('not found.');
+        exit;
+    }
+
+    $data = $s->get($filename, '0-');
+    file_put_contents('data/' . $filename, $data);
+
+    $lines = explode("\n", $data);
+    $timestamp = current(explode('<>', end($lines)));
+    touch('data/' . $filename, $timestamp);
+}
+
+get_thread($s, 'thread_503250');
 
 /*
 foreach ($files as $file) {
