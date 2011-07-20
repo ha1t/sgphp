585b6c0dda870135505a31f15c6b79e62d6cc8b5
ha1t <halt.feits@gmail.com>
2011-07-20 16:55:15 +0900

    get(),head()で、$timestamp,$idをうけとれるように

--- server.php
+++ server.php
@@ -26,8 +26,18 @@ file_put_contents($log_file, $log);
 $command = $pathinfo[1];
 if ($command === 'join') {
     $server->join($pathinfo[2], $_SERVER['REMOTE_ADDR']);
+} else if ($command === 'get' || $command == 'head') {
+    $id = false;
+    if (isset($pathinfo[4])) {
+        $id = $pathinfo[4];
+    }
+    $server->$command($pathinfo[2], $pathinfo[3], $id);
 } else if ($command === 'update') {
-    $server->update($pathinfo[2], $pathinfo[3], $pathinfo[4], $pathinfo[5]);
+    $node = $pathinfo[5];
+    if ($node[0] == ':') {
+        $node = $_SERVER['REMOTE_ADDR'] . $node;
+    }
+    $server->update($pathinfo[2], $pathinfo[3], $pathinfo[4], $node);
 } else if (isset($pathinfo[2])) {
     $server->$command($pathinfo[2]);
 } else {
