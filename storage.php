147a27f4e054ad0c9939042dd486e1a1904ac64d
ha1t <halt.feits@gmail.com>
2012-03-12 19:14:40 +0900

    modelに処理を委譲

--- storage.php
+++ storage.php
@@ -22,4 +22,70 @@ class storage
         krsort($items);
         return $items;
     }
+
+    public static function getThread($title)
+    {
+        $filename = 'data/thread_' . strtoupper(bin2hex($title));
+
+        if (!file_exists($filename)) {
+            return false;
+        }
+
+        $raw_data = file_get_contents($filename);
+
+        $threads = array();
+        $remove_ids = array();
+        $lines = explode("\n", $raw_data);
+        foreach ($lines as $line) {
+            $parts = explode('<>', $line);
+            $thread = array(
+                'name' => '',
+                'timestamp' => array_shift($parts),
+                'id' => array_shift($parts),
+                'body' => '',
+            );
+            $thread['datetime'] = date('Y-m-d H:i:s', $thread['timestamp']);
+
+            while(count($parts) > 0) {
+                if (strpos($parts[0], 'name:') === 0) {
+                    $thread['name'] = array_shift($parts);
+                } else if (strpos($parts[0], 'mail:') === 0) {
+                    $thread['mail'] = array_shift($parts);
+                } else if (strpos($parts[0], 'body:') === 0) {
+                    $thread['body'] = substr(array_shift($parts), 5);
+                } else if (strpos($parts[0], 'remove_stamp:') === 0) {
+                    array_shift($parts);
+                } else if (strpos($parts[0], 'remove_id:') === 0) {
+                    $remove_ids[] = str_replace('remove_id:', '', array_shift($parts));
+                } else if (strpos($parts[0], 'attach:') === 0) {
+                    $thread['attach'] = substr(array_shift($parts), 7);
+                } else if (strpos($parts[0], 'suffix:') === 0) {
+                    $thread['suffix'] = substr(array_shift($parts), 7);
+                } else if (strpos($parts[0], 'sign:') === 0) {
+                    // implement yet
+                    array_shift($parts);
+                } else if (strpos($parts[0], 'pubkey:') === 0) {
+                    // implement yet
+                    array_shift($parts);
+                } else if (strpos($parts[0], 'target:') === 0) {
+                    // implement yet
+                    array_shift($parts);
+                } else {
+                    var_dump(__LINE__,$parts);exit;
+                    // throw new Exception();
+                }
+            }
+
+            $threads[$thread['id']] = $thread;
+        }
+
+        // remove_id指定されたレスの削除
+        foreach ($remove_ids as $remove_id) {
+            if (isset($threads[$remove_id])) {
+                unset($threads[$remove_id]);
+            }
+        }
+
+        return $threads;
+    }
 }
--- thread.php
+++ thread.php
@@ -3,76 +3,13 @@
 // @TODO latest50件ごと表示
 // @TODO 特定レスの表示
 
+require_once dirname(__FILE__) . '/storage.php';
+
 // @note スレッド内のレス番は、識別子の先頭8桁。
-function get_thread($title)
-{
-    $filename = 'data/thread_' . strtoupper(bin2hex($title));
-
-    if (!file_exists($filename)) {
-        return false;
-    }
-
-    $raw_data = file_get_contents($filename);
-
-    $threads = array();
-    $remove_ids = array();
-    $lines = explode("\n", $raw_data);
-    foreach ($lines as $line) {
-        $parts = explode('<>', $line);
-        $thread = array(
-            'name' => '',
-            'timestamp' => array_shift($parts),
-            'id' => array_shift($parts),
-            'body' => '',
-        );
-        $thread['datetime'] = date('Y-m-d H:i:s', $thread['timestamp']);
-
-        while(count($parts) > 0) {
-            if (strpos($parts[0], 'name:') === 0) {
-                $thread['name'] = array_shift($parts);
-            } else if (strpos($parts[0], 'mail:') === 0) {
-                $thread['mail'] = array_shift($parts);
-            } else if (strpos($parts[0], 'body:') === 0) {
-                $thread['body'] = substr(array_shift($parts), 5);
-            } else if (strpos($parts[0], 'remove_stamp:') === 0) {
-                array_shift($parts);
-            } else if (strpos($parts[0], 'remove_id:') === 0) {
-                $remove_ids[] = str_replace('remove_id:', '', array_shift($parts));
-            } else if (strpos($parts[0], 'attach:') === 0) {
-                $thread['attach'] = substr(array_shift($parts), 7);
-            } else if (strpos($parts[0], 'suffix:') === 0) {
-                $thread['suffix'] = substr(array_shift($parts), 7);
-            } else if (strpos($parts[0], 'sign:') === 0) {
-                // implement yet
-                array_shift($parts);
-            } else if (strpos($parts[0], 'pubkey:') === 0) {
-                // implement yet
-                array_shift($parts);
-            } else if (strpos($parts[0], 'target:') === 0) {
-                // implement yet
-                array_shift($parts);
-            } else {
-                var_dump(__LINE__,$parts);exit;
-                // throw new Exception();
-            }
-        }
-
-        $threads[$thread['id']] = $thread;
-    }
-
-    // remove_id指定されたレスの削除
-    foreach ($remove_ids as $remove_id) {
-        if (isset($threads[$remove_id])) {
-            unset($threads[$remove_id]);
-        }
-    }
-
-    return $threads;
-}
 
 $pathinfo = explode('/', $_SERVER['PATH_INFO']);
 $title = $pathinfo[1];
-$threads = get_thread($title);
+$threads = Storage::getThread($title);
 
 if (isset($pathinfo[2])) {
     $ident_prefix = $pathinfo[2];
@@ -91,6 +28,17 @@ if ($threads === false) {
 <html>
 <head>
   <title><?php echo $title; ?></title>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8020577-13']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 <body>
   <h1><?php echo $title; ?></h1>
