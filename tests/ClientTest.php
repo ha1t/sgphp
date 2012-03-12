0a782e7a1b9ce13b01ed30073629ebe70731ed59
ha1t <halt.feits@gmail.com>
2012-03-12 18:15:19 +0900

    テストをphpunitへ移行

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -14,4 +14,13 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $result = $client->head($thread_name, '-');
         $this->assertTrue(is_string($result)); 
     }
+
+    public function testUpdate() 
+    {
+        //$my_server = 'sgphp.project-p.jp';
+        //$s = new Shingetsu_Client($my_server);
+
+        //$re = file_get_contents('http://sgphp.project-p.jp/server.php/update/thread_E69CAC/1311085121/f9e7ab5f2dc9b4293fa8a17a1cdc9c94/sgphp.project-p.jp:80+server.php');
+        //var_dump($re);
+    }
 }
