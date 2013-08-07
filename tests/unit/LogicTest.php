fecf320a48c82cacad834dfcfab35399b43b6c33
ha1t <halt.feits@gmail.com>
2013-08-07 13:23:46 +0900

    不要なコメントを削除

--- tests/unit/LogicTest.php
+++ tests/unit/LogicTest.php
@@ -1,16 +1,10 @@
 <?php
-/**
- *
- *
- */
-
 class LogicTest extends PHPUnit_Framework_TestCase
 {
     public function testDecode()
     {
         $thread_name = 'thread_E99B91E8AB87';
         $name = pack('H*', str_replace('thread_', '', $thread_name));
-
-        $this->assertEquals($name, '雑談'); 
+        $this->assertEquals($name, '雑談');
     }
 }
